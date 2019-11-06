3c16545ac7f4b7d7c7082f317920750cef8ba599
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2019-11-06 18:56:26 +0200

    🌊 updated database

--- project.sql
+++ project.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 18, 2017 at 08:50 ص
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Gazdă: 127.0.0.1
+-- Timp de generare: nov. 06, 2019 la 05:55 PM
+-- Versiune server: 10.1.36-MariaDB
+-- Versiune PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `project`
+-- Bază de date: `project`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cart`
+-- Structură tabel pentru tabel `cart`
 --
 
 CREATE TABLE `cart` (
@@ -36,10 +38,17 @@ CREATE TABLE `cart` (
   `u_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Eliminarea datelor din tabel `cart`
+--
+
+INSERT INTO `cart` (`c_id`, `p_id`, `product_name`, `product_price`, `file_name`, `quantity`, `u_id`) VALUES
+(1, 1, 'Vaporesso Revenger X', '350', 'res_e6eed49ee8f4a7daf14fac3d1be90161_full.jpg', 1, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pending_orders`
+-- Structură tabel pentru tabel `pending_orders`
 --
 
 CREATE TABLE `pending_orders` (
@@ -56,10 +65,17 @@ CREATE TABLE `pending_orders` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Eliminarea datelor din tabel `pending_orders`
+--
+
+INSERT INTO `pending_orders` (`po_id`, `u_id`, `cart_total`, `bill_fname`, `bill_lname`, `bill_add`, `bill_city`, `bill_email`, `bill_phone`, `status`, `date`) VALUES
+(1, 2, '1350', 'da', 'da', 'da', 'khi', 'da', 'd032190918421', 'delivered', '2019-11-06 16:15:07');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Structură tabel pentru tabel `products`
 --
 
 CREATE TABLE `products` (
@@ -67,16 +83,23 @@ CREATE TABLE `products` (
   `product_name` varchar(100) NOT NULL,
   `product_category` varchar(100) NOT NULL,
   `product_price` varchar(100) NOT NULL,
-  `product_des` varchar(1000) NOT NULL,
+  `product_des` mediumtext NOT NULL,
   `file_name` varchar(100) NOT NULL,
   `file_path` varchar(100) NOT NULL,
   `file_ext` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Eliminarea datelor din tabel `products`
+--
+
+INSERT INTO `products` (`p_id`, `product_name`, `product_category`, `product_price`, `product_des`, `file_name`, `file_path`, `file_ext`) VALUES
+(1, 'Vaporesso Revenger X', 'Starter Kits', '350', 'The Revenger X is an incredible device, with incredible power, that\'s been specifically designed by the folks at Vaporesso to cover all of the expectations you have for an excellent kit, and go above-and-beyond. The touch-screen takes over for the traditional button-style format, without sacrificing the interactive capabilities of the menu functions. In addition, the kit features the new and improved NRG Tank, which features its GT coils that promise for a high volume of vapor and incredible flavor! \r\n\r\nIt is recommended that you use a Max VG blend with this tank.\r\n\r\nParameters:\r\n\r\nRevenger X Mod Size: 45mm * 89mm * 28mm\r\nVaporesso NRG Tank Size: 26.5mm * 56mm\r\nCell Type: Two 18650 cell (Not Included)\r\nTank Capacity: 5ml\r\nThread Type: 510 Thread\r\nPower Range: 5W - 220W\r\nRated Resistance Range: 0.05?-5? \r\nRated Temperature Range: 100-315°C/200-600°F(TC mode)\r\nKit Contains:\r\n\r\n(1) Revenger X MOD\r\n(1) Vaporesso NRG Tank\r\n(1) Extra Pyrex Glass Tube\r\n(1) GT8 Core 0.15? Quad coil Pre-installed in the tank\r\n(1) GT4 Core 0.15? Quad coil\r\n(1) USB Charging Cable\r\n(1) User Manual\r\nSpare Parts\r\n*This Device Requires (2) 18650 Battery*', 'res_e6eed49ee8f4a7daf14fac3d1be90161_full.jpg', 'C:/xampp/htdocs/e-commerce-master/uploads/', '.jpg');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `purchase_history`
+-- Structură tabel pentru tabel `purchase_history`
 --
 
 CREATE TABLE `purchase_history` (
@@ -89,10 +112,17 @@ CREATE TABLE `purchase_history` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Eliminarea datelor din tabel `purchase_history`
+--
+
+INSERT INTO `purchase_history` (`ph_id`, `oh`, `p_name`, `p_price`, `p_qty`, `u_id`, `date`) VALUES
+(1, 1, 'Vaporesso Revenger X', '350', '1', 2, '2019-11-06 14:55:13');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structură tabel pentru tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -106,56 +136,58 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Eliminarea datelor din tabel `users`
 --
 
 INSERT INTO `users` (`u_id`, `fname`, `lname`, `username`, `email`, `password`, `status`) VALUES
-(1, 'Abdul', 'Samad', 'asamad', 'as@as.com', 'admin', 'admin'),
-(2, 'Muhammad', 'Sameer', 'sameer', 'ms@ms.com', 'user', '');
+(1, 'Root', 'Admin', 'admin', 'admin@ecigora.com', 'admin', 'admin'),
+(2, 'Rosu', 'Razvan', 'yungr', 'yungr@gmail.com', 'yungr', '');
 
 --
--- Indexes for dumped tables
+-- Indexuri pentru tabele eliminate
 --
 
 --
--- Indexes for table `cart`
+-- Indexuri pentru tabele `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`c_id`);
 
 --
--- Indexes for table `pending_orders`
+-- Indexuri pentru tabele `pending_orders`
 --
 ALTER TABLE `pending_orders`
   ADD PRIMARY KEY (`po_id`);
 
 --
--- Indexes for table `products`
+-- Indexuri pentru tabele `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`p_id`);
 
 --
--- Indexes for table `purchase_history`
+-- Indexuri pentru tabele `purchase_history`
 --
 ALTER TABLE `purchase_history`
   ADD PRIMARY KEY (`ph_id`);
 
 --
--- Indexes for table `users`
+-- Indexuri pentru tabele `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`u_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pentru tabele eliminate
 --
 
 --
--- AUTO_INCREMENT for table `purchase_history`
+-- AUTO_INCREMENT pentru tabele `purchase_history`
 --
 ALTER TABLE `purchase_history`
-  MODIFY `ph_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ph_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
