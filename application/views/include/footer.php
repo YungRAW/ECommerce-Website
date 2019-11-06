d1f5f668c6037af8ea7ff018cff92c628d0f66b4
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2019-11-06 18:53:08 +0200

    🌊 v1.0 added shopping cart, admin rights, crud on products

---
+++ application/views/include/footer.php
@@ -0,0 +1,52 @@
+
+
+<div class="footer-top-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-about-us">
+                    <h2>E Cigora <span>Vape Shop</span></h2>
+                    <p>Be safe, be vape!</p>
+                    <div class="footer-social">
+                        <strong>Follow us on :</strong>
+                        <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div  class="col-md-3 pull-right col-sm-6">
+                <div  class="footer-newsletter">
+                    <h2  class="footer-wid-title">Newsletter</h2>
+                    <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                    <div class="newsletter-form">
+                        <form action="#">
+                            <input type="email" placeholder="Type your email">
+                            <input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End footer top area -->
+
+<div class="footer-bottom-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="copyright">
+                    <p>&copy;  2019-2020 ECigora. All Rights Reserved. Coded with <i class="fa fa-heart"></i> by <a href="https://www.facebook.com/yungrrrr" target="_blank">Răzvan Roșu</a></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div> <!-- End footer bottom area -->
---
+++ application/views/index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ECigora</title>
+    <link rel="shortcut icon" href="<?php echo base_url()?>assets/img/logo.png" />
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/style.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php
+include "include/head.php";
+?>
+
+<div class="slider-area">
+    <div class="zigzag-bottom"></div>
+    <div id="slide-list" class="carousel carousel-fade slide" data-ride="carousel">
+        <div class="slide-bulletz">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ol class="carousel-indicators slide-indicators">
+                            <li data-target="#slide-list" data-slide-to="0" class="active"></li>
+                            <li data-target="#slide-list" data-slide-to="1"></li>
+                            <li data-target="#slide-list" data-slide-to="2"></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="single-slide">
+                    <div class="slide-bg slide-one"></div>
+                    <div class="slide-text-wrapper">
+                        <div class="slide-text">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6 col-md-offset-6">
+                                        <div class="slide-content">
+                                            <h2>De ce țigara electronică?</h2>
+                                            <p>Țigara electronică poate înlocui cu succes țigara clasică. Avantajele unei țigări electronice, față de cele clasice, sunt multiple: nu conține gudron, nu conține monoxid de carbon, nu are un miros neplăcut și costă considerabil mai puțin. În plus, țigara electronică poate fi folosită în orice spațiu unde fumatul clasic este interzis. Oferta Smokemania conține cele mai avansate și noi țigări electronice la cele mai bune prețuri. Țigările electronice de pe www.smokemania.ro vin în întâmpinarea fumătorilor care doresc să scape de viciu într-un mod elegant, sănătos și plăcut. Adoptând o țigară electronică, în detrimentul țigărilor clasice, poți obține o mulțime de beneficii și, într-un final, dacă reduci doza de nicotină pe care o inhalezi, poți ajunge să renunți de tot la obiceiul fumatului.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="single-slide">
+                    <div class="slide-bg slide-two"></div>
+                    <div class="slide-text-wrapper">
+                        <div class="slide-text">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6 col-md-offset-6">
+                                        <div class="slide-content">
+                                            <h2>Mods</h2>
+                                            <p>Mods come in a variety of styles, sizes, maximum power outputs, colours & finishes. A Mod usually contains the battery that provide the power to your tank or atomizer. So whether it's something small & portable you're sure to find a mod to suit you.</p>
+                                            <a href="" class="readmore">Learn more</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="single-slide">
+                    <div class="slide-bg slide-three"></div>
+                    <div class="slide-text-wrapper">
+                        <div class="slide-text">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6 col-md-offset-6">
+                                        <div class="slide-content">
+                                            <h2>Starter Kits</h2>
+                                            <p>Starter kits from incredible brands like SMOK, Suorin, Vaporesso, Voopoo and more, make buying a great vape device easy. They include all the basics you need for a product that just works straight out of the box. Kits contain a mod, tank, coils and replacement parts; or a device and pods. To get vaping, all you need to add is some vape juice and a battery (if it's not built in). As a leading online destination for all things vape, ECigora partners world’s top vaping brands so that you can find your perfect vape kit. We've got all your needs covered with our range of kits: elegant vape pens, simple pod systems and versatile box mod kits.</p>
+                                            <a href="" class="readmore">Learn more</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div> <!-- End slider area -->
+
+<div class="promo-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo">
+                    <i class="fa fa-refresh"></i>
+                    <p>14 Days Return</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo">
+                    <i class="fa fa-flash"></i>
+                    <p>Flashy Deals</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo">
+                    <i class="fa fa-lock"></i>
+                    <p>Secured Payments</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo">
+                    <i class="fa fa-gift"></i>
+                    <p>New Products Weekly</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End promo area -->
+
+<div class="maincontent-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="latest-product">
+                    <h2 class="section-title">Latest Products</h2>
+                    <div class="product-carousel">
+
+                        <?php
+                        if(isset($prod)){
+                            foreach ($prod as $row){
+                                ?>
+                                <div class="single-product">
+                                    <div class="product-f-image">
+                                        <img style='height:250px; width:300px' src="<?php echo base_url()?>uploads/<?php echo $row->file_name ?>" alt="">
+                                        <div class="product-hover">
+                                            <a href="<?php echo base_url()?>index.php/Cart?id=<?php echo $row->p_id ?>" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                            <a href="<?php echo base_url() ?>index.php/Welcome/single?id=<?php echo $row->p_id ?>" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                        </div>
+                                    </div>
+
+                                    <h2><a href="single-product.php"><?php echo $row->product_name ?></a></h2>
+
+                                    <div class="product-carousel-price">
+                                        <ins><?php echo $row->product_price ?> Lei</ins>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End main content area -->
+
+<div class="brands-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="brand-wrapper">
+                    <h2 class="section-title">Brands</h2>
+                    <div class="brand-list">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/1.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/2.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/3.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/4.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/5.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/6.png" alt="">
+                        <img src="<?php echo base_url() ?>assets/img/backgrounds/7.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include "include/footer.php";
+?>
+
+<!-- Latest jQuery form server -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+<!-- Bootstrap JS form CDN -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<!-- jQuery sticky menu -->
+<script src="<?php echo base_url() ?>assets/js/owl.carousel.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery.sticky.js"></script>
+
+<!-- jQuery easing -->
+<script src="<?php echo base_url() ?>assets/js/jquery.easing.1.3.min.js"></script>
+
+<!-- Main Script -->
+<script src="<?php echo base_url() ?>assets/js/main.js"></script>
+</body>
+</html>
